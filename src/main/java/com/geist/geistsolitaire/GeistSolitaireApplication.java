e226436b09eb48933baed916b5e69cc86eafaeb0
LeeHyunGi <geist9110@naver.com>
2022-10-23 12:39:14 +0900

    spring boot + react initializing connect react main page to spring boot main controller

---
+++ src/main/java/com/geist/geistsolitaire/GeistSolitaireApplication.java
@@ -0,0 +1,13 @@
+package com.geist.geistsolitaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeistSolitaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeistSolitaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geist/geistsolitaire/GeistSolitaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geist.geistsolitaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeistSolitaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
